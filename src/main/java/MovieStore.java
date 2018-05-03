9f276d4d547296c9bbdc1a08bde1d3235ec99245
Brian Wong <brian.lw.wong@gmail.com>
2018-05-03 09:54:02 -0500

    MovieStore updated

--- src/main/java/MovieStore.java
+++ src/main/java/MovieStore.java
@@ -4,29 +4,37 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MovieStore {
-    List<Movie> movies = new LinkedList<Movie>();
-
-    public List<Movie> findByPartialTitle(String partialTitle) {
-        List<Movie> result = new LinkedList<Movie>();
-        for (Movie movie : movies) {
-            if(movie.title().toLowerCase().contains(partialTitle.toLowerCase())){
-                result.add(movie);
-            }
-        }
-        return result;
-    }
+    List<Movie> movies = new LinkedList<>();
 
     public void add(Movie movie) {
         movies.add(movie);
     }
 
-    public List<Movie> findByDirector(String director) {
-        List<Movie> result = new LinkedList<Movie>();
+    public List<Movie> findByPartialTitle(final String partialTitle) {
+        return findBy(movie -> movie.title().toUpperCase().contains(partialTitle.toUpperCase()));
+    }
+
+    public List<Movie> findByDirector(final String director) {
+        return findBy(movie -> movie.director().equals(director));
+
+    }
+
+    public List<Movie> findByReleaseYear(final int from, final int to) {
+        return findBy(movie -> movie.releaseYear() > from && movie.releaseYear() < to);
+
+    }
+
+    private List<Movie> findBy(Predicate predicate) {
+        List<Movie> result = new LinkedList<>();
         for (Movie movie : movies) {
-            if(movie.director().equals(director)){
+            if(predicate.matches(movie)){
                 result.add(movie);
             }
         }
         return result;
+    }
+
+    interface Predicate{
+        boolean matches(Movie movie);
     }
 }
